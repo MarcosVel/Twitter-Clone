8abe9cde79d608c7917d95c703284e86f09a23c8
MarcosVel <marcos.veloso@sempreceub.com>
2021-03-08 23:49:23 -0300

    add imageInput

--- src/Components/TweetBox/TweetBox.js
+++ src/Components/TweetBox/TweetBox.js
@@ -10,6 +10,11 @@ function TweetBox() {
             <Avatar src='https://instagram.fplu1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/124595979_190868415857383_3289167799779777978_n.jpg?tp=1&_nc_ht=instagram.fplu1-1.fna.fbcdn.net&_nc_ohc=BCjvgNi6cVUAX_nW16a&oh=903f4ec9746cb206afa6f79f4c59667c&oe=606ED973' />
             <input type="text" placeholder='O que está acontecendo?' />
           </div>
+          <input
+            className='tweetBox_imageInput'
+            placeholder='Enter image URL'
+            type='text'
+          />
           <Button className="tweetBox_tweetButton">Tweetar</Button>
         </form>
       </div>
