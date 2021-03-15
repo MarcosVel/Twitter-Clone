fc5864751a09f26480697ee792e744f080c37581
MarcosVel <marcos.veloso@sempreceub.com>
2021-03-15 00:22:01 -0300

    post section ok firebase

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -27,12 +27,12 @@ function Feed() {
 
       {posts.map(post => (
         <Post 
-          displayName='Marcos Veloso'
-          username='marquin'
-          verified={true}
-          text='YOOO working'
-          avatar='https://instagram.fplu1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/124595979_190868415857383_3289167799779777978_n.jpg?tp=1&_nc_ht=instagram.fplu1-1.fna.fbcdn.net&_nc_ohc=BCjvgNi6cVUAX_nW16a&oh=903f4ec9746cb206afa6f79f4c59667c&oe=606ED973'
-          image='https://i.pinimg.com/564x/de/f6/96/def69643889ee29e232637646e839064.jpg'
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          avatar={post.avatar}
+          image={post.image}
         />
       ))}
       
