9cbe0fcc6002710c28238f6de029b902c4327451
MarcosVel <marcos.veloso@sempreceub.com>
2021-03-24 18:50:23 -0300

    project finished in video

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -4,6 +4,7 @@ import TweetBox from '../TweetBox/TweetBox';
 import Post from '../Posts/Post';
 import { useState, useEffect } from 'react';
 import db from '../../firebase';
+import FlipMove from 'react-flip-move';
 
 function Feed() {
   const [ posts, setPosts ] = useState([]);
@@ -25,16 +26,19 @@ function Feed() {
 
       <div className="separator"></div>
 
-      {posts.map(post => (
-        <Post
-          displayName={ post.displayName }
-          username={ post.username }
-          verified={ post.verified }
-          text={ post.text }
-          avatar={ post.avatar }
-          image={ post.image }
-        />
-      )) }
+      <FlipMove>
+        { posts.map(post => (
+          <Post
+            key={ post.text }
+            displayName={ post.displayName }
+            username={ post.username }
+            verified={ post.verified }
+            text={ post.text }
+            avatar={ post.avatar }
+            image={ post.image }
+          />
+        )) }
+      </FlipMove>
 
     </main>
   )
