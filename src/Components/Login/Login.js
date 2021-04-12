7afcc11f4a5d1c8b351e21cdacc18f223dcc726c
MarcosVel <marcos.veloso@sempreceub.com>
2021-04-11 21:05:24 -0300

    add btns in login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,6 @@
 import './Login.css';
 import { AiOutlineTwitter } from "react-icons/ai";
+import { Button } from '@material-ui/core';
 
 function Login() {
   return (
@@ -12,6 +13,11 @@ function Login() {
           <AiOutlineTwitter size='52' className='login__logoIcon' />
           <h1>Acontecendo agora</h1>
           <h2>Inscreva-se no Twitter hoje mesmo.</h2>
+          <Button variant='outlined' className='login_btn btn_entrar'>Entrar</Button>
+          <a href="https://www.linkedin.com/in/marcosveloso99/" target="_blank" rel="noopener noreferrer" title="Marcos Veloso - LinkedIn">
+            <Button variant='outlined' className='login_btn btn_dev'>Desenvolvedor</Button>
+
+          </a>
         </div>
       </section>
     </div>
